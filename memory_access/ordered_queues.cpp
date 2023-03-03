840c2f72d30e99da926c9603b5db0477a217f5f7
Osman El-Ghotmi <osman.elghotmi@gmail.com>
2023-03-03 16:58:51 -0500

    ordered queues

---
+++ memory_access/ordered_queues.cpp
@@ -0,0 +1,77 @@
+#include <CL/sycl.hpp>
+#include <array>
+#include <iostream>
+#include <assert.h>
+
+constexpr int SIZE = 256;
+
+// prints device name
+template<typename Queue_type>
+void print_device(Queue_type& Q){
+  std::cout << "DEVICE: "
+            << Q.get_device().template get_info<sycl::info::device::name>()
+            << "\nVENDOR: "
+            << Q.get_device().template get_info<sycl::info::device::vendor>()
+            << "\n" << std::endl;
+}
+
+int main(){
+  // establishing gpu for device queue
+  sycl::queue Q{sycl::gpu_selector_v};
+  print_device(Q);
+
+  // allocating memory on host
+  std::array<int, SIZE> Arr_host;
+
+  // initializing host array
+  for(int i = 0; i < SIZE; ++i){
+    Arr_host[i] = i;
+  }
+
+  // initializing devide buffer
+  sycl::buffer Arr_buffer(Arr_host);
+
+  // incrementing array values on device by 1
+  Q.submit([&](sycl::handler &h){
+    sycl::accessor Arr_accessor(Arr_buffer, h);
+
+    h.parallel_for(SIZE, [=](sycl::id<1> idx){
+      Arr_accessor[idx]++;
+    });
+  });
+
+  Q.wait();
+
+  // doubling array values on device
+  Q.submit([&](sycl::handler &h){
+    sycl::accessor Arr_accessor(Arr_buffer, h);
+
+    h.parallel_for(SIZE, [=](sycl::id<1> idx){
+      Arr_accessor[idx]*=2.0;
+    });
+  });
+
+  Q.wait();
+
+  // incrementing array values on device by 3
+  Q.submit([&](sycl::handler &h){
+    sycl::accessor Arr_accessor(Arr_buffer, h);
+
+    h.parallel_for(SIZE, [=](sycl::id<1> idx){
+      Arr_accessor[idx] += 3.0;
+    });
+  });
+
+  Q.wait();
+
+  sycl::host_accessor Arr_host_accessor(Arr_buffer);
+
+  // checking results
+  for(int i = 0; i < SIZE; ++i){
+    assert(Arr_host_accessor[i] = (i + 1.0)*2.0 + 3.0);
+  }
+
+  std::cout << "The results are correct!" << std::endl;
+
+  return 0;
+}
